1ee5adb81e5e517abb28cbfbb26004d0c52f7481
noobstonks <androfarlinno@gmail.com>
2021-08-16 13:23:19 +0700

    error routing and internal server error handler

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//catatan:
 // const express = require ("express");
 // const app = express();
 // const port = 3000;
@@ -5,17 +6,44 @@
 // app.get("/", (req, res) => res.send("hello world"));
 // app.listen(port, () => console.log(`example app listening at http://localhost:${port}`));
 
+
+// app.listen(3000, function(){
+// console.log("Server Nyala")
+// })
+
+//harus nyala terminalnya - berjalan terus
+
 const express = require ("express");
 const app = express();
 const port = 3000;
 
-//app.listen(port, ()=> console.log(`example app listening at http://localhost:${port}`));
+const morgan = require ("morgan");
+app.use(morgan("dev"));
 
+
+//middleware routing
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 const router = require ("./router");
+//tambah error routing
+app.get ("/iniError", (req, res) =>{
+    iniError
+})
 app.use (router);
+
+//internal server error handler
+app.use(function (err, req, res, next){
+    console.error(err)
+    res.status(500).json({
+        status: "fail",
+        errors: err.message
+    })
+})
+
+
+
+
 
 app.get("/endpointlain", (req, res)=>{
     res.send("lain")
@@ -27,15 +55,15 @@ app.listen(port, () =>{
 } )
 
 
+//application level middleware
 const logger = (req,res, next) => {
     console.log(`${req.method} ${req.url}`)
     next()
 }
-
 app.use(logger)
 
 
-
+//routing
 app.get ('/products', (req, res)=> {
     res.json([
         "apple",
@@ -58,13 +86,3 @@ app.get("/orders", (req, res) =>{
         },
     ])
 })
-
-
-
-
-
-// app.listen(3000, function(){
-// console.log("Server Nyala")
-// })
-
-//harus nyala terminalnya - berjalan terus
